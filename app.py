5ff8c99b1b7d1fbbf3451a96def78541c2b53e34
prog17-cloud <mehtaparag45@gmail.com>
2025-03-08 19:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,12 +40,14 @@ st.write(data)
 data_train = pd.DataFrame(data.Close[0: int(len(data) * 0.80)])
 data_test = pd.DataFrame(data.Close[int(len(data) * 0.80): len(data)])
 
-pas_100_days = data_train.tail(100)
-data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
 
-# Use the loaded scaler to transform both train and test data consistently
-data_train_scaled = scaler.transform(data_train) #Corrected line.
-data_test_scaled = scaler.transform(data_test)
+pas_100_days = data_train.tail(100)
+data_train = pd.concat([pas_100_days, data_test], ignore_index=True)
+data_train_scale = scaler.fit_transform(data_train)
+
+
 
 st.subheader('Price vs MA50')
 ma_50_days = data.Close.rolling(50).mean()
