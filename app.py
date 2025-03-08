6782401bff4292f52dda65c5a10e3ef1741d86a9
prog17-cloud <mehtaparag45@gmail.com>
2025-03-08 19:14:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+
 import streamlit as st
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow import keras
+from keras.models import load_model
 
-model = tf.saved_model.load('Stock Predictions Model.keras')
+model = tf.keras.model.load_model('Stock Predictions Model.keras')
 
 st.header('Stock Market Predictor')
 
